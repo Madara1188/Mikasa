1b443120eedc143fd1ffd4839f86a693b13e19d8
Madara <147428837+Madara1188@users.noreply.github.com>
2025-07-24 22:59:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -131,7 +131,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text='ᴛʀʏ ᴀɢᴀɪɴ',
+                    text='⚡️ᴛʀʏ ᴀɢᴀɪɴ⚡️',
                     url=f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
